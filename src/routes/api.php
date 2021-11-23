45e5e90c1dd5df5b5b4dfa979434120f89a11c1c
chisom <chisom.e@credpal.com>
2021-11-23 23:42:51 +0100

    update api

--- src/routes/api.php
+++ src/routes/api.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
+
+    Route::get('/api/expense', function() {
+    	return 'Expense package';
+    });
+
 Route::group([
     'namespace' => 'Credpal\Expense\Http\Controllers',
     'prefix' => 'api/expense',
