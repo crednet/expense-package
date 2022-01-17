d21cf1d1983e9d33f899e13e466ad37755a3948d
Geoslim <iedomwande@yahoo.com>
2022-01-17 13:29:40 +0100

    refactor expense

---
+++ src/Traits/BIllTransactionTrait.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Credpal\Expense\Traits;
+
+use Credpal\Expense\Utilities\Enum;
+
+trait BIllTransactionTrait
+{
+	public string $billsTransaction;
+
+	public function logBillsTransactions(
+		$accountId,
+		$walletId,
+		$amount,
+		$reference,
+		$type,
+		$walletType,
+		$recipientNumber
+	): void {
+		$this->getBillsTransactionInstance();
+		$this->billsTransaction::transactionLogger(
+			$accountId,
+			$walletId,
+			$amount,
+			$reference,
+			$type,
+			ENUM::PENDING,
+			$walletType === ENUM::CREDIT ? 'credpal_card' : 'credpal_cash',
+			$recipientNumber
+		);
+	}
+
+	public function getBillsTransactionInstance()
+	{
+		$billTransactionModel = config('expense.bill_transactions_model');
+		$this->billsTransaction = new $billTransactionModel();
+		return $this->billsTransaction;
+	}
+}
