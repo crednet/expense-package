5bc6e9d46fa6d5655be7b22b2cb35c65ecd4838a
Geoslim <iedomwande@yahoo.com>
2022-02-08 15:00:55 +0100

    move middleware checkers for bills

--- src/routes/api.php
+++ src/routes/api.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Middleware\{
+	CheckAirtimeDailyUsage,
+	PostNoDebitRestriction,
+	Blacklisted,
+	TrustedDeviceMiddleware,
+	TransactionPinMiddleware
+};
 
     Route::get('/api/expense', function() {
     	return 'Expense package';
@@ -27,22 +33,31 @@ Route::group([
 		Route::get('biller-by-category/{category}', 'BaxiController@getBillerByCategory');
 
 		Route::get('airtime-providers', 'BaxiController@getAirtimeProviders');
-		Route::post('airtime-request', 'BaxiController@airtimeRequest');
 
 		Route::get('databundle-providers', 'BaxiController@getDatabundleProviders');
 		Route::get('provider-bundles/{provider}', 'BaxiController@getBundleByProvider');
-		Route::post('databundle-request', 'BaxiController@dataBundleRequest');
 
 		Route::post('verify-account-details', 'BaxiController@verifyAccountDetails');
 
 		Route::get('cabletv-providers', 'BaxiController@getCabletvProviders');
 		Route::get('multichoice-bundles-list/{provider}', 'BaxiController@getMultichoiceBundles');
 		Route::post('multichoice/addons', 'BaxiController@getMultichoiceAddons');
-		Route::post('multichoice-request', 'BaxiController@multichoiceRequest');
 
 		Route::get('electricity-billers', 'BaxiController@getElectricityBillers');
 		Route::post('verify-electricity-user', 'BaxiController@verifyElectricityUser');
-		Route::post('electricity-request', 'BaxiController@electricityRequest');
+
+		Route::group(['middleware' => [
+			Blacklisted::class,
+			CheckAirtimeDailyUsage::class,
+			PostNoDebitRestriction::class,
+			TrustedDeviceMiddleware::class,
+			TransactionPinMiddleware::class
+		]], function () {
+			Route::post('airtime-request', 'BaxiController@airtimeRequest');
+			Route::post('databundle-request', 'BaxiController@dataBundleRequest');
+			Route::post('multichoice-request', 'BaxiController@multichoiceRequest');
+			Route::post('electricity-request', 'BaxiController@electricityRequest');
+		});
 	});
 
 	Route::group([
