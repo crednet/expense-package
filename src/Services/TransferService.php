a5d4e2173779b1fffcbff208b948047b69605ad2
Damilare Anjorin <damilareanjorin1@gmail.com>
2022-06-21 11:43:37 +0100

    chore: update

--- src/Services/TransferService.php
+++ src/Services/TransferService.php
@@ -3,51 +3,56 @@
 namespace Credpal\Expense\Services;
 
 use App\UserProfile;
-use Credpal\Expense\Contract\ExpenseContract;
 use Credpal\Expense\Exceptions\ExpenseException;
-use Credpal\Expense\Traits\ExpenseError;
 use Credpal\Expense\Utilities\Enum;
-use Illuminate\Http\Response;
 use Illuminate\Support\Collection;
-use Illuminate\Support\Facades\Http;
 
 class TransferService extends ExpenseProcess
 {
-	protected Collection $credentials;
+    protected Collection $credentials;
 
-	public function __construct($credentials)
-	{
-		parent::__construct($credentials);
-	}
+    public function __construct($credentials)
+    {
+        parent::__construct($credentials);
+    }
 
-	/**
-	 * @return array
-	 * @throws ExpenseException
-	 */
-	public function makeTransfer(): array
-	{
-		$this->setDestinationAccountNumberAndUserBankCode();
-		logger('transfering');
-		logger($this->credentials);
-		DuplicateTransactionService::checkDuplicateTransfer(
-			Enum::WALLET_TYPE_CASH,
-			$this->credentials['wallet_id'],
-			$this->credentials['amount']
-		);
-		$this->expenseRequestBody = [
-			'account_name' => $this->credentials['account_name'],
-			'account_number' => $this->credentials['account_number'],
-			'bank_code' => $this->credentials['bank_code'],
-		];
+    /**
+     * @return array
+     * @throws ExpenseException
+     */
+    public function makeTransfer(): array
+    {
+        $this->setDestinationAccountNumberAndUserBankCode();
+        logger('transfering');
+        DuplicateTransactionService::checkDuplicateTransfer(
+            Enum::WALLET_TYPE_CASH,
+            $this->credentials['wallet_id'],
+            $this->credentials['amount']
+        );
+        $this->expenseRequestBody = [
+            'account_name' => $this->credentials['account_name'],
+            'account_number' => $this->credentials['account_number'],
+            'bank_code' => $this->credentials['bank_code'],
+        ];
 
-		return $this->initiateTransaction(ENUM::TRANSFER, 'transfers');
-	}
+        return $this->initiateTransaction(ENUM::TRANSFER, 'transfers');
+    }
 
-	private function setDestinationAccountNumberAndUserBankCode(): void
-	{
-		$result = $this->creditCardTransaction->checkBankAccount(null, true);
-		$this->credentials['account_number'] = $result['account_number'];
-		$this->credentials['bank_code'] = $result['bank_name'];
-		$this->credentials['account_name'] = $result['account_name'];
-	}
+    public function withoutAccount()
+    {
+        return null;
+    }
+
+    public function withTransfer(): bool
+    {
+        return true;
+    }
+
+    private function setDestinationAccountNumberAndUserBankCode(): void
+    {
+        $result = $this->creditCardTransaction->checkBankAccount($this->withoutAccount(), $this->withTransfer());
+        $this->credentials['account_number'] = $result['account_number'];
+        $this->credentials['bank_code'] = $result['bank_name'];
+        $this->credentials['account_name'] = $result['account_name'];
+    }
 }
